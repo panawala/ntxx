da46eb7614129eda163679ebf1bf47f3fd3f67ee
wei782566232@gmail.com <wei782566232@gmail.com@dbf1b5f2-0962-0774-807a-199a33905f80>
2012-07-13 08:45:21 +0000

    修改了画图接口 潘伟龙

---
+++ trunk/DxfLib/Entity/DataCenter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DxfLib.Entity
+{
+    public class DataCenter
+    {
+        public OrderEntity OrderEntity { get; set; }
+        public SectionEntity SectionEntity { get; set; }
+        public List<string> Configurations { get; set; }
+
+    }
+}
