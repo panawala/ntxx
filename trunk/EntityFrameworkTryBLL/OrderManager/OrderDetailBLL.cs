7ff87534bc1f525f0c14fd330ba8807cfdad959c
wei782566232@gmail.com <wei782566232@gmail.com@dbf1b5f2-0962-0774-807a-199a33905f80>
2012-07-23 04:34:58 +0000

    订单详细信息 By 张宇航

--- trunk/EntityFrameworkTryBLL/OrderManager/OrderDetailBLL.cs
+++ trunk/EntityFrameworkTryBLL/OrderManager/OrderDetailBLL.cs
@@ -13,12 +13,12 @@ namespace EntityFrameworkTryBLL.OrderManager
         //获取一个订单详情;
         public static List<orderDetailInfo> GetOrderDetail(int OrderInfoID)
         {
-            using (var context =new AnnonContext())
+            using (var context = new AnnonContext())
             {
                 try
                 {
-                    var od=context.orderDetailInfoes
-                        .Where(s =>s.OrderInfoId==OrderInfoID)
+                    var od = context.orderDetailInfoes
+                        .Where(s => s.OrderInfoId == OrderInfoID)
                         .ToList();
 
                     return od;
@@ -33,18 +33,18 @@ namespace EntityFrameworkTryBLL.OrderManager
         //获取所有订单详细信息;
         public static List<orderDetailInfo> GetAllOrderDetail()
         {
-            using(var context =new AnnonContext())
+            using (var context = new AnnonContext())
             {
                 try
                 {
-                    var od=context.orderDetailInfoes
+                    var od = context.orderDetailInfoes
                         .ToList();
 
                     return od;
                 }
-                catch(Exception e)
+                catch (Exception e)
                 {
-                   return null;
+                    return null;
                 }
             }
         }
@@ -56,8 +56,8 @@ namespace EntityFrameworkTryBLL.OrderManager
             {
                 try
                 {
-                    var od=context.orderDetailInfoes
-                    .Where(s =>s.orderDetailInfoID==OrderInfoID)
+                    var od = context.orderDetailInfoes
+                    .Where(s => s.orderDetailInfoID == OrderInfoID)
                     .First();
 
                     context.orderDetailInfoes.Remove(od);
@@ -72,13 +72,14 @@ namespace EntityFrameworkTryBLL.OrderManager
         //删除所有详细订单信息;
         public static int DeleteAllOrderDetail()
         {
-            using(var context=new AnnonContext())
+            using (var context = new AnnonContext())
             {
                 try
                 {
-                    var od=context.orderDetailInfoes;
+                    var od = context.orderDetailInfoes;
 
-                    foreach(var odd in od){
+                    foreach (var odd in od)
+                    {
                         context.orderDetailInfoes.Remove(odd);
                     }
                     return context.SaveChanges();
@@ -89,31 +90,31 @@ namespace EntityFrameworkTryBLL.OrderManager
                     return -1;
                 }
             }
-            }
+        }
 
         //插入详细订单信息;
-        public static int InsertOrderDetail(int OrderDInfoID,int OrderID,int OrderNO,string qty,string proDes,string Tag,string LPrice,string RPrince,string CPrice)
+        public static int InsertOrderDetail(int OrderDInfoID, int OrderID, int OrderNO, string qty, string proDes, string Tag, string LPrice, string RPrince, string CPrice)
         {
             using (var context = new AnnonContext())
             {
                 try
                 {
-                    orderDetailInfo od=new orderDetailInfo
+                    orderDetailInfo od = new orderDetailInfo
                     {
-                        orderDetailInfoID=OrderDInfoID,
-                        OrderInfoId=OrderID,
-                        OrderDetailNo=OrderNO,
-                        Qty=qty,
-                        tag=Tag,
-                        ProDes=proDes,
-                        listPrice=LPrice,
-                        RepPrice=RPrince,
-                        custPrice=CPrice
+                        orderDetailInfoID = OrderDInfoID,
+                        OrderInfoId = OrderID,
+                        OrderDetailNo = OrderNO,
+                        Qty = qty,
+                        tag = Tag,
+                        ProDes = proDes,
+                        listPrice = LPrice,
+                        RepPrice = RPrince,
+                        custPrice = CPrice
                     };
                     context.orderDetailInfoes.Add(od);
                     return context.SaveChanges();
                 }
-                catch(Exception e)
+                catch (Exception e)
                 {
                     return -1;
                 }
@@ -150,25 +151,25 @@ namespace EntityFrameworkTryBLL.OrderManager
         //复制详细订单信息;
         public static int CopyOrderDetail(int OrderDtIfID)
         {
-            using (var context=new AnnonContext())
+            using (var context = new AnnonContext())
             {
                 try
                 {
-                    var od1=context.orderDetailInfoes
-                        .Where(s => s.OrderInfoId==OrderDtIfID)
+                    var od1 = context.orderDetailInfoes
+                        .Where(s => s.OrderInfoId == OrderDtIfID)
                         .Last();
 
-                    orderDetailInfo od2=new orderDetailInfo
+                    orderDetailInfo od2 = new orderDetailInfo
                     {
-                        orderDetailInfoID=od1.orderDetailInfoID+1,
-                        OrderInfoId=od1.OrderInfoId+1,
-                        OrderDetailNo=od1.OrderDetailNo+1,
-                        Qty=od1.Qty,
-                        custPrice=od1.custPrice,
-                        listPrice=od1.listPrice,
-                        RepPrice=od1.RepPrice,
-                        tag=od1.tag,
-                        ProDes=od1.ProDes
+                        orderDetailInfoID = od1.orderDetailInfoID + 1,
+                        OrderInfoId = od1.OrderInfoId + 1,
+                        OrderDetailNo = od1.OrderDetailNo + 1,
+                        Qty = od1.Qty,
+                        custPrice = od1.custPrice,
+                        listPrice = od1.listPrice,
+                        RepPrice = od1.RepPrice,
+                        tag = od1.tag,
+                        ProDes = od1.ProDes
                     };
 
                     context.orderDetailInfoes.Add(od2);
@@ -176,10 +177,32 @@ namespace EntityFrameworkTryBLL.OrderManager
                 }
                 catch (System.Exception ex)
                 {
-                	return -1;
+                    return -1;
                 }
             }
         }
+
+        //模拟  插入详细订单信息;
+        public static int InsertOD(int OrderID, int OrderDID, string proDes)
+        {
+            using (var context = new AnnonContext()) 
+            {
+                try
+                {
+                    orderDetailInfo od = new orderDetailInfo();
+
+                    od.OrderInfoId = OrderID;
+                    od.orderDetailInfoID = OrderDID;
+                    od.ProDes = proDes;
+
+                    context.orderDetailInfoes.Add(od);
+                    return context.SaveChanges();
+                }
+                catch (System.Exception ex)
+                {
+                    return -1;
+                }
+            }
         }
     }
-
+}
