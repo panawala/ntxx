6bc0c7ce64fb256f54eb6633643f7490ea125f73
wei782566232@gmail.com <wei782566232@gmail.com@dbf1b5f2-0962-0774-807a-199a33905f80>
2012-07-24 16:07:56 +0000

    订单的OrderDetail部分功能基本完成 By 张宇航

--- Model/Order/orderDetailInfo.cs
+++ Model/Order/orderDetailInfo.cs
@@ -9,15 +9,14 @@ namespace Model.Order
     public class orderDetailInfo
     {
         public int OdDetlNum { get; set; }  //订单排序号;
-        public int orderDetailInfoID { get; set; }//订单详细信息类自身唯一ID
-        public int OrderInfoId { get; set; }//对应订单的ID
-        public int OrderDetailNo { get; set; }//订单详细信息项的排列序号；
         public string Qty { get; set; }
         public string ProDes { get; set; }
         public string tag { get; set; }
         public string listPrice { get; set; }
         public string RepPrice { get; set; }
         public string custPrice { get; set; }
-        
+        public int orderDetailInfoID { get; set; }//订单详细信息类自身唯一ID
+        public int OrderDetailNo { get; set; }//订单详细信息项的排列序号；
+        public int OrderInfoId { get; set; }//对应订单的ID
     }
 }
