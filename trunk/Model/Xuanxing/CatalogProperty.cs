15cea7e61b24591c19c300871eda887a526d2ed4
wei782566232@gmail.com <wei782566232@gmail.com@dbf1b5f2-0962-0774-807a-199a33905f80>
2012-07-26 05:47:15 +0000

    增加了一些选型操作 潘伟龙】

---
+++ trunk/Model/Xuanxing/CatalogProperty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model.Xuanxing
+{
+    /// <summary>
+    /// 左边gridview的绑定项
+    /// </summary>
+    public class CatalogProperty
+    {
+        public string CatalogName { get; set; }
+        public string PropertyName { get; set; }
+    }
+}
